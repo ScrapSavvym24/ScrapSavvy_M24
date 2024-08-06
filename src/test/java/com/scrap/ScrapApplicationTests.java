cefb9732118c0ae5ae2320337ea04798bab45603
Mahendra Sunil Waghchaure <162361329+MahendraSWaghchaure@users.noreply.github.com>
2024-08-06 22:54:25 +0530

    First Backend push

---
+++ src/test/java/com/scrap/ScrapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scrap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

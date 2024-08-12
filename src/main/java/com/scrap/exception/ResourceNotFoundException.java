6834fa02f6e7a5d6aaf2b33d1ce8804d223d888c
ScrapSavvym24 <scrapsavvy.m24@gmail.com>
2024-08-12 20:14:12 +0530

    Revert "Aarti Bhasme"

--- src/main/java/com/scrap/exception/ResourceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.scrap.exception;
-
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
-
